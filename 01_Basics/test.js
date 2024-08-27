8261acddac62ac5e40148db3fee70f5c7102efa0
Engr Saddam Janjua <saddam.janjua2001@gmail.com>
2024-08-27 10:24:03 +0000

    code

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,2 +1,15 @@
 console.log("Saddam")
 console.log(2+2)
+
+// const myFunction = function(){
+//     console.log("data entered")
+// }
+//let score = 33
+//let score = "33"  
+let score = "33abc"  
+console.log(typeof score)
+console.log (typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); 
+console.log(valueInNumber);
