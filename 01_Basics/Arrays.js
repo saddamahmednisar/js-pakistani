c8aa28d648614ace334994e58e990e06bd9911cc
Engr Saddam Janjua <saddam.janjua2001@gmail.com>
2024-08-28 12:48:17 +0000

    array half

--- 01_Basics/Arrays.js
+++ 01_Basics/Arrays.js
@@ -1,1 +1,8 @@
 console.log("***********Arrays***************")
+
+
+const myArr = [0,1,2,3,4,5] // first array num basis
+const Heros = ["me","leo","grambell"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[0])
