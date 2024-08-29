8cf243c092d9a4a51fe26706fd26bbfb2d4d0de8
Engr Saddam Janjua <saddam.janjua2001@gmail.com>
2024-08-29 12:31:47 +0000

    working on objects

--- 01_Basics/object1.js
+++ 01_Basics/object1.js
@@ -1,7 +1,9 @@
 //objects
+
 //there are two ways to declare object one  is constructor and sefcond is literal 
 //singleton concept when make with constructor it is singleton but with literal not singleton
 //object.create // we can also make object bys using this method
+
 //object literals
 
 const mySym=Symbol("key1") // symbol usage for keys
@@ -21,4 +23,4 @@ const JsUser ={
 console.log(JsUser.email); // METHOD 1 (commonly used)
 //console.log(JsUser["email"]); // Another Method when we name a key string in "" the first method odesnt access it
 console.log (JsUser["full name"]);//this is declare in above object full name
-console.log(JsUser[mySym])   
+console.log(JsUser[mySym]);  
