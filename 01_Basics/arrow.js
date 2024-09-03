d4b714dc1bc0e15db96c81f1b5af0529252c3aee
Engr Saddam Janjua <saddam.janjua2001@gmail.com>
2024-09-03 08:10:23 +0000

    arrow intresting errors solved

--- 01_Basics/arrow.js
+++ 01_Basics/arrow.js
@@ -4,12 +4,42 @@ const user ={
     username:"saddam",
     price: 999,
 
-
     welcomeMessage: function(){
         console.log(`${this.username}, welcome to the website`)
+        //console.log(this);
+        
     }
 }
-user.welcomeMessage();
-user.username="sam";
-user.welcomeMessage();
 
+// user.welcomeMessage();
+// user.username="Ahmed";
+// user.welcomeMessage();
+//console.log(this); // here it is an empty object in global but in browser window is a global object
+
+
+//  const chai = function () {
+//     let username = "janjua"
+//   console.log(this.username); // when we call(this) inside it give alot of value but when outside it gives empty object
+//  }
+
+//we cant use this in single function that are not with object
+//  const chai = ()=>{
+//     let username = "janjua"
+//     console.log(this); 
+//  }
+
+//  chai();
+
+
+
+// basic arrow function
+// const addTwoNum = (n1, n2)=>{
+//        return n1+n2;
+// }
+// console.log(addTwoNum(5,5))
+
+
+// implicit return
+// const addTwoNum = (n1, n2)=> n1+n2;
+const addTwoNum = (n1, n2)=> (n1+n2)
+console.log(addTwoNum(5,5))
