0266e58149598415768e37da2996f2a4fd98b94e
Engr Saddam Janjua <saddam.janjua2001@gmail.com>
2024-09-05 12:41:54 +0000

    working on primewith single loop

--- Iterations/prime_num.js
+++ Iterations/prime_num.js
@@ -1,47 +1,66 @@
 //// /////////////////prime number by single number input//////////////////////
-function primeNumber(num){
-  if(num<=1){
-     return `${num} this is not applicable`;
-  }
+// function primeNumber(num){
+//   if(num<=1){
+//      return `${num} this is not applicable`;
+//   }
 
-  for(let i = 2; i<=Math.sqrt(num); i++){
-      if(num%i===0){
-          return `${num} is not a prime number`;   
-      } 
-      
-  }
-      return `${num} is a prime number`;  
-      
-}
-const result=primeNumber(7);
-console.log("result", result);
+//   for(let i = 2; i<=Math.sqrt(num); i++){
+//       if(num%i===0){
+//           return `${num} is not a prime number`;   
+//       } 
+
+//   }
+//       return `${num} is a prime number`;  
+
+// }
+// const result=primeNumber(7);
+// console.log("result", result);
 
 
 //////////////////// prime number check by giving range//////////////////////////////
 
-function primeNumCheck(start, end){
-  for(let i=start ; i<=end ; i++){
-    isPrime= true;
-    if(i<2){
-      continue;
-    }
+// function primeNumCheck(start, end) {
+//   for (let i = start; i <= end; i++) {
+//     isPrime = true;
+//     if (i < 2) {
+//       continue;
+//     }
 
-    for(let j=2; j<=Math.sqrt(i); j++){
-      if(i%j===0){
-        isPrime=false;
-        break;
+//     for (let j = 2; j <= Math.sqrt(i); j++) {
+//       if (i % j === 0) {
+//         isPrime = false;
+//         break;
+//       }
+//     }
+//     if (isPrime) {
+//       console.log(i)
+//     }
+//   }
+
+// }
+// console.log(primeNumCheck(1, 100));
+
+///////////////////////////////  with one loop ////////////////////////////////////////
+
+function primeNumCheck(start, end) {
+  console.log("loop entered",start, end)
+  let result=start;
+  console.log("loop entered1")
+  let isPrime = false;
+  console.log("loop entered2")
+
+  for (let i = start; i <= end; i++) {
+  console.log("loop entered3", i, result)
+
+      if ( % i === 0) {
+        console.log("loop entered4")
+        isPrime = false;
       }
-    }
-    if(isPrime){
-      console.log(i)
-    }
-  }
+      else{
+        isPrime = true;
+        console.log(isPrime,result)
+      }
 
 }
-console.log(primeNumCheck(1,100));
-
-
-
-
-
-
+}
+console.log(primeNumCheck(1, 100))
