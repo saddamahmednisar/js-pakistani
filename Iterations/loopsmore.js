609a86bed9562350d51eff4773092f0a9e5a678f
Engr Saddam Janjua <saddam.janjua2001@gmail.com>
2024-09-10 07:59:30 +0000

    while loop and loop more

---
+++ Iterations/loopsmore.js
@@ -0,0 +1,36 @@
+//// for of loop /////////////////
+
+
+//[" "," "," "]
+//[{},{},{}]
+
+
+console.log("Using For-of Loop")
+
+// const arr = ["Raja","saddam","ahmed","nisar","janjua"]
+
+
+// for (const num of arr) {
+//     console.log(num)
+// }
+
+//for of on string
+// const greetings = "Hello_Janjua_Sahb"
+
+
+// for (const greet of greetings) {
+//     console.log(`Each character is ${greet}`)
+// } 
+
+
+// ////////use Map//////////////
+
+
+const map = new Map();
+map.set("PAK","Pakistan")
+map.set("USA","United States of America")
+map.set("KSA","Kingdom of Saudi Arabia")
+map.set("UAE","United Arab Emirates")
+//map.set("PAK","Pakistan") //if we add pak again then it not accept it cz it is us4ed for uniqueness and correct order
+// and it is remain same in that order which we entered
+console.log(map);
---
+++ Iterations/whiles_loop.js
@@ -0,0 +1,55 @@
+////////////////////// while and do while loop///////////////////
+
+// while loop
+// console.log("WHILE LOOP")
+// let i=0;   // here we  do initialization
+// while (i<=10) {   // here is condition of loop
+//     console.log(`Value of i is ${i}`);
+//     i=i+2;  // here is an increment
+    
+// }
+// while loop using array
+// console.log("array using while loop")
+// let myArray =["superman","batman","spiderman"]
+// let arr =0;
+// while (arr<myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr= arr+1;
+// }
+
+
+
+
+// Do while loop
+
+
+// console.log("DO-WHILE LOOP")
+// let j=0;
+// do {
+//     console.log(`Value of j using do while ${j}`);
+//     j=j+3;
+// } while (j<=10);
+
+
+// console.log("Array using Do-while");
+
+// let myArray =["Janjuas world","Rajpoot era","Raja's raj everywhere"]
+// let arr =0;
+//  do {
+//     console.log(`The ${myArray[arr]}`);
+//     arr=arr+1;
+    
+//  } while (arr<myArray.length);
+
+
+
+ // if your condition is wrong but do while runs for 1 time
+
+
+ console.log("DO WHILE EXAMPLE");
+
+ let score =11;
+ do {
+    console.log(`Score is ${score}`)
+    score++;
+ } while (score<=10);
