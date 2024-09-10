5eecb3f59741b9a369f4d16cc852e4d5896dadfc
Engr Saddam Janjua <saddam.janjua2001@gmail.com>
2024-09-10 12:20:07 +0000

    complate for each loop

--- Iterations/foreach.js
+++ Iterations/foreach.js
@@ -1,2 +1,60 @@
 //////////////FOR EACH LOOP////////////////////
 console.log("For Each Loop");
+
+
+const coding = ["cpp","js","py","css"]
+
+// coding.forEach(function (item){
+//     console.log(item);
+    
+// })
+
+
+//by arrow function with for each
+
+
+// coding.forEach((item) => {
+//     console.log(item);
+    
+// });
+
+
+// function PrintMe(item){
+//  console.log(item);
+ 
+// }
+//  coding.forEach(PrintMe)
+
+//  coding.forEach((item, index, arr) => {
+//         console.log(item, index, arr);
+//  })
+
+
+
+
+// here we can access data which is in array and with in array we have objects and we get data from objects 
+console.log("use of for each loop to get object item from array");
+
+
+ const myCoding =[
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "C++",
+        languageFileName: "cpp"
+    },
+ ]
+
+ myCoding.forEach((item)=>{
+    console.log(item.languageName);
+    
+ })
+ 
+
+ // here we all hacve functions for of, in, each to get data from array ,objects and from both
